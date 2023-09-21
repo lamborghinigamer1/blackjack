d554402452fbdf2793e0002a9b3b17e5abfa7560
lamborghinigamer1 <61702082+lamborghinigamer1@users.noreply.github.com>
2023-09-21 11:02:50 +0200

    Added to main a simple way to add players on Run

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,21 @@ mod player;
 
 fn main() {
     println!("Welcome to blackjack");
-
+    println!("How many players will be playing?");
     let mut dealer = dealer::Dealer::new(blackjack::Blackjack, deck::Deck::new());
-    dealer.add_player(player::Player::new("Lambo".to_string()));
-    dealer.add_player(player::Player::new("Tijmoe".to_string()));
+    let mut playersamount = String::new();
+    std::io::stdin().read_line(&mut playersamount).unwrap();
+
+    let players_amount: i32 = match playersamount.trim().parse() {
+        Ok(n) => n,
+        Err(e) => panic!("Invalid number: {}", e),
+    };
+    for _ in 0..players_amount {
+        println!("Player's name:");
+        let mut playername = String::new();
+        std::io::stdin().read_line(&mut playername).unwrap();
+        dealer.add_player(player::Player::new(playername.trim().to_string()));
+    }
 
     dealer.play_game();
 }
